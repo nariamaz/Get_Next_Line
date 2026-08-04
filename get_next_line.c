71d64c77dff4d2be344e88b1dcec6af5c1c00066
nariamaz <marianasantossales@gmail.com>
2026-08-04 20:02:31 -0300

    develops logic with arrays

--- get_next_line.c
+++ get_next_line.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maridos- <maridos-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: maridos- <maridos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 15:04:16 by maridos-          #+#    #+#             */
-/*   Updated: 2026/08/04 00:09:38 by maridos-         ###   ########.fr       */
+/*   Updated: 2026/08/04 20:01:05 by maridos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdlib.h>
 
-void add_node(t_list *list)
+/*void add_node(t_list *list)
 {
     t_list *new_node;
     
@@ -24,7 +25,7 @@ void add_node(t_list *list)
     new_node->next = NULL;
     new_node->str = NULL;
     list->next = new_node;
-}
+}*/
 
 int str_search (char *str)
 {
@@ -45,12 +46,20 @@ int str_search (char *str)
 
 char *get_next_line(int fd)
 {
-    char* buf_read [BUFFER_SIZE];
+    char* buf_read;
+    int processed_bytes;
     
+    processed_bytes = 0;
+    buf_read = NULL;
+    buf_read = malloc(sizeof(char) * (BUFFER_SIZE +1));
+    if (!buf_read)
+        return (NULL);
     if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-    read (fd, buf_read, (BUFFER_SIZE));
+    processed_bytes = (read (fd, buf_read, (BUFFER_SIZE)));
+    if (processed_bytes > 0)
+        str_search ();
+    
 
-
-    return ("error");
+    return ();
 }
