d8cf1a3be0b6e517354fc00c616f5dcb63171372
nariamaz <272286362+nariamaz@users.noreply.github.com>
2026-08-04 00:11:44 -0300

    inserts ft search

--- get_next_line.c
+++ get_next_line.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maridos- <maridos-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: maridos- <maridos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 15:04:16 by maridos-          #+#    #+#             */
-/*   Updated: 2026/08/01 23:19:59 by maridos-         ###   ########.fr       */
+/*   Updated: 2026/08/04 00:09:38 by maridos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,31 @@ void add_node(t_list *list)
     list->next = new_node;
 }
 
+int str_search (char *str)
+{
+    int position;
+    int i;
+
+    position = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == '\n')
+            position = i;    
+        i++; 
+    }
+    return (position);
+}
+
 
 char *get_next_line(int fd)
 {
+    char* buf_read [BUFFER_SIZE];
     
-    int i = 0;
-    if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > 10000000)
+    if (fd < 0 || BUFFER_SIZE <= 0)
         return (NULL);
-    char var [42];
-    read (fd, var, BUFFER_SIZE);
-    printf ("valor de var: \n%s\n", var); 
-    printf ("lina 1\n"); 
-    while (var[i] != '\n')
-        {
-            printf("%c", var[i]);
-            i++;
-        }
-    printf("\n");
+    read (fd, buf_read, (BUFFER_SIZE));
 
-    printf ("linha 2 \n"); 
-    i++;
-    while (var[i] != '\n')
-        {
-            printf("%c", var[i]);
-            i++;
-        }
-    printf("\n");
+
     return ("error");
 }
